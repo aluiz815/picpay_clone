87e4ec8fda1934deb2f28a71e2c1abef0f72f748
Andre Luiz <aluiz815@gmail.com>
2020-07-29 08:43:38 -0300

    NotificationScreen Finished

--- src/screens/Notification/index.js
+++ src/screens/Notification/index.js
@@ -1,16 +1,74 @@
 import React from 'react';
 
-import { Header,HeaderText,HeaderButton,ButtonLabel } from './styles';
+import { Wrapper,Header,HeaderText,HeaderButton,ButtonLabel,
+  NotificationContainer,
+  NotificationLabel,
+  NotificationHour,
+  NotificationLine
+ } from './styles';
 
 const Notification = () => {
   return (
+    <>
+    <Wrapper>  
     <Header>
       <HeaderText>Notificações</HeaderText>
       <HeaderButton>
         <ButtonLabel>Configurar</ButtonLabel>
       </HeaderButton>
     </Header>
-    
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    <NotificationContainer>
+        <NotificationLabel>Picpay + Rede = Amor. Agora você pode pagar com PicPay nas maquininhas Rede.Pague Já</NotificationLabel>
+        <NotificationHour>Ontem ás 17:19</NotificationHour>
+        <NotificationLine></NotificationLine>
+    </NotificationContainer>
+    </Wrapper>  
+    </>
   );
 }
 
--- src/screens/Notification/styles.js
+++ src/screens/Notification/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 
+export const Wrapper = styled.ScrollView``;
 export const Header = styled.View`
   background:#E0E0E0;
   justify-content:space-between;
@@ -21,4 +22,22 @@ export const ButtonLabel = styled.Text`
 color:#4CAF50;
 font-size:14px;
 `;
-
+export const NotificationContainer = styled.TouchableOpacity`
+flex-direction:column;
+justify-content:center;
+padding:15px;
+`;
+export const NotificationLabel = styled.Text`
+font-size:16px;
+margin-bottom:10px;
+font-weight:500;
+`;
+export const NotificationHour = styled.Text`
+font-size:12px;
+color:#666;
+`;
+export const NotificationLine = styled.View`
+width:100%;
+border:1px solid #E0E0E0;
+margin-top:10px;
+`
